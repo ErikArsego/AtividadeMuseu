139aef7eca07a3c4ff2e4c073415b832dab485b6
ErikArsego <144289443+ErikArsego@users.noreply.github.com>
2024-05-22 19:47:55 -0300

    Delete MuseuDeArte/AtividadeBonus directory
    Excluido para adicionar nova versão atualizada

--- MuseuDeArte/AtividadeBonus/src/ObraDeArte.java
+++
@@ -1,9 +0,0 @@
-import java.io.Serializable;
-
-public abstract class ObraDeArte implements Serializable {
-    private String titulo;
-    private String artista;
-    private int anoDeCriacao;
-    private String localizacao;
-
-    public ObraDeArte(String titulo, String artista, int anoDeCriacao, String localizacao
